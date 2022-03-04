6b0fdb0c99661ee9692de49697c5ffc7229c724e
themordhon <romdhonm12@gmail.com>
2022-03-04 10:28:17 +0700

    API run

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,22 @@ import Map from "./components/Map/Map";
 
 const App = () => {
   const [places, setPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState({ lat: 0, lng: 0 });
+  const [bounds, setBounds] = useState({});
 
   useEffect(() => {
-    getPlacesData().then((data) => {
-      console.log(data);
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoordinates({ lat: latitude, lng: longitude });
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    getPlacesData(bounds.sw, bounds.ne).then((data) => {
       setPlaces(data);
     });
-  }, []);
+  }, [coordinates, bounds]);
 
   return (
     <>
@@ -23,10 +32,14 @@ const App = () => {
       <Header />
       <Grid container spacing={3} style={{ width: "100%" }}>
         <Grid item xs={12} md={4}>
-          <List />
+          <List places={places} />
         </Grid>
         <Grid item xs={12} md={8}>
-          <Map />
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+          />
         </Grid>
       </Grid>
     </>
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -12,40 +12,10 @@ import PlaceDetails from "../PlaceDetails/PlaceDetails";
 
 import useStyles from "./styles";
 
-const List = () => {
+const List = ({ places }) => {
   const classes = useStyles();
   const [type, setType] = useState("restaurants");
   const [rating, setRating] = useState("");
-
-  const places = [
-    {
-      name: "cool place",
-    },
-    {
-      name: "Best Burger",
-    },
-    {
-      name: "McDonald",
-    },
-    {
-      name: "cool place",
-    },
-    {
-      name: "Best Burger",
-    },
-    {
-      name: "McDonald",
-    },
-    {
-      name: "cool place",
-    },
-    {
-      name: "Best Burger",
-    },
-    {
-      name: "McDonald",
-    },
-  ];
 
   return (
     <div className={classes.container}>
